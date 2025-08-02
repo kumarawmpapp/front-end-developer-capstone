b5ad6e9858da0360bb898f92111298b09d8f1e00
Pradeep Kumara <kumarawmpapp@gmail.com>
2025-08-02 23:31:24 +0530

    Refined featues

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,47 @@
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomePage from './pages/HomePage'
-import BookingPage from './pages/BookingPage'
-import ConfirmedBooking from './pages/ConfirmedBooking'
-import './App.css'
 import {
-  Route,
+  Route, 
   Routes
-} from "react-router-dom"
+} from 'react-router-dom';
+import pages from './utils/pages';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/Home';
+import Bookings from './components/pages/Bookings';
+import ConfirmedBooking from './components/pages/Bookings/ConfirmedBooking';
+import NotFound from './components/pages/NotFound';
+import UnderConstruction from './components/pages/UnderConstruction';
 
-function App() {
-
+const App = () => {
   return (
     <>
-     <Header />
-     <Routes> 
-      <Route path="/" exact element={<HomePage />}></Route>
-      <Route path="/booking" element={<BookingPage />}></Route> 
-      <Route path="/confirmed" element={<ConfirmedBooking />}></Route>
-    </Routes>
-     <Footer />
+      <Layout>
+        <Routes>
+          <Route path={pages.get('home').path} element={<Home />} />
+          <Route 
+            path={pages.get('about').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('menu').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path={pages.get('bookings').path} element={<Bookings />} />
+          <Route 
+            path={pages.get('confirmedBooking').path} 
+            element={<ConfirmedBooking />} 
+          />
+          <Route 
+            path={pages.get('orderOnline').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route 
+            path={pages.get('login').path} 
+            element={<UnderConstruction />} 
+          />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Layout>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
